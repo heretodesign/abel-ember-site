bc9b935304e98661b8ddb86152ffeab2adf9d5cc
heretodesign <senwelo.works@gmail.com>
2020-01-24 17:08:30 +0800

    added org user route to handle routing to each image

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.route('organization', { path: '/organizations/:org' });
+  this.route('organization', { path: '/organizations/:org' }, function() {
+    this.route('user', { path: '/users/:login' });
+  });
 });
 
 export default Router;
